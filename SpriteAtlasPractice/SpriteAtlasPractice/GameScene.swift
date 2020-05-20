6eeab1f59a7f1a312bb12f6d2471fa5e084373c6
Thomas Cacciatore <tcacciatore597@gmail.com>
2020-05-20 14:46:02 -0400

    Playing with Sprite Atlas to create a running dog animation in the Games Udemy course.

---
+++ SpriteAtlasPractice/SpriteAtlasPractice/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  SpriteAtlasPractice
+//
+//  Created by Thomas Cacciatore on 5/20/20.
+//  Copyright © 2020 Thomas Cacciatore. All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    
+    let dogSpriteNode = SKSpriteNode(imageNamed: "Run0")
+    var dogFrames = [SKTexture]()
+    
+    override func didMove(to view: SKView) {
+        
+        dogSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(dogSpriteNode)
+        
+        let textureAtlas = SKTextureAtlas(named: "Dog Frames")
+        for index in 0..<textureAtlas.textureNames.count {
+            let textureName = "Run" + String(index)
+            dogFrames.append(textureAtlas.textureNamed(textureName))
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !dogSpriteNode.hasActions() {
+            dogSpriteNode.run(SKAction.repeatForever(SKAction.animate(with: dogFrames, timePerFrame: 0.1)))
+        } else {
+            dogSpriteNode.removeAllActions()
+        }
+        
+    }
+        
+}
